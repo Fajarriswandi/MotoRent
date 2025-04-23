e837d32a4c5670ce683a1e0542d0b29a6c669fa7
Fajar Riswandi <fajar.riswandi05@gmail.com>
2025-04-23 20:54:33 +0700

    Fix missing URL facade import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,22 +2,18 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-
-    public function register(): void
-    {
-        //
-    }
-
     public function boot()
     {
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
-        Paginator::useBootstrapFive(); // atau Paginator::useBootstrapFour();
+
+        Paginator::useBootstrapFive();
     }
 }
